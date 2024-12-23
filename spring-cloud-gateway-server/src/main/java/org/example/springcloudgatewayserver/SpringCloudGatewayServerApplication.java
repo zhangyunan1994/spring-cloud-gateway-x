b91beeef3233bf68f4e316673185464466b69b4c
zhangyunan <zyndev@gmail.com>
2024-12-23 20:44:07 +0800

    add

---
+++ spring-cloud-gateway-server/src/main/java/org/example/springcloudgatewayserver/SpringCloudGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.springcloudgatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCloudGatewayServerApplication.class, args);
+  }
+
+}
---
+++ spring-cloud-gateway-server/src/test/java/org/example/springcloudgatewayserver/SpringCloudGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springcloudgatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
